7ecd6dc0468888e5b4b7153ec98e8d79083bb40a
Vladimir PEROVIC <vladimir.perovic@gmail.com>
2026-07-30 12:47:10 +0200

    refactor(web): switch production entry to component dashboard

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import DashboardApp from "./DashboardApp";
 import DashboardLogs from "./components/DashboardLogs";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <DashboardApp />
     <DashboardLogs />
   </StrictMode>,
 );
