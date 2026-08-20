fff9ef513b47a56534e67ccb8ea4897d44134fb0
Vladimir PEROVIC <vladimir.perovic@gmail.com>
2026-08-20 08:55:41 +0200

    chore(v0.1.5): align demo source version

--- web/src/lib/demoApi.ts
+++ web/src/lib/demoApi.ts
@@ -116,7 +116,7 @@ function liveSystem() {
   trafficTick += 1;
   return {
     status: "Connected",
-    version: "v0.1.4-demo",
+    version: "v0.1.5-demo",
     revision: config.revision,
     update_trust_configured: true,
     recovery_required: false,
